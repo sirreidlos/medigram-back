dc382b1b5539b1c33dcb2b742b278b8a6ac98be7
Erik Sanjaya <erik.sanjaya25@gmail.com>
2025-04-16 20:09:07 +0700

    move components from `main.rs` to `lib.rs`

---
+++ src/lib.rs
@@ -0,0 +1,95 @@
+pub mod auth;
+pub mod canonical_json;
+pub mod error;
+pub mod model;
+pub mod protocol;
+pub mod route;
+pub mod schema;
+
+use axum::{
+    Router,
+    extract::FromRef,
+    routing::{delete, get, post, put},
+};
+use protocol::Nonce;
+use route::consultation::{get_diagnoses, get_prescriptions, get_symptoms};
+
+use std::time::Duration;
+use uuid::Uuid;
+
+use moka::sync::Cache;
+use sqlx::Pool;
+use sqlx::postgres::Postgres;
+use tower_http::cors::{Any, CorsLayer};
+
+use route::{
+    allergy::{add_allergy, get_allergies, remove_allergy},
+    consultation::{add_consultation, get_consultations},
+    doctor_profile::{get_doctor_profile, set_doctor_profile},
+    purchase::{add_purchase, get_purchases},
+    request_nonce,
+    user::get_user,
+    user_detail::{get_user_detail, set_user_detail},
+    user_measurement::{add_user_measurement, get_user_measurements},
+};
+
+// 7d
+const NONCE_TTL: Duration = Duration::from_secs(7 * 24 * 60 * 60);
+
+#[derive(Clone)]
+pub struct AppState {
+    pub nonce_cache: Cache<Nonce, ()>,
+    pub db_pool: Pool<Postgres>,
+    pub recognized_session_id: Cache<String, Uuid>,
+}
+
+impl FromRef<AppState> for Cache<String, Uuid> {
+    fn from_ref(input: &AppState) -> Self {
+        input.recognized_session_id.clone()
+    }
+}
+
+impl FromRef<AppState> for Pool<Postgres> {
+    fn from_ref(input: &AppState) -> Self {
+        input.db_pool.clone()
+    }
+}
+
+impl FromRef<AppState> for Cache<Nonce, ()> {
+    fn from_ref(input: &AppState) -> Self {
+        input.nonce_cache.clone()
+    }
+}
+
+pub fn app(state: AppState) -> Router {
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_headers(Any);
+
+    Router::new()
+        .route("/allergy", get(get_allergies))
+        .route("/allergy", post(add_allergy))
+        .route("/allergy", delete(remove_allergy))
+        .route("/consultation", get(get_consultations))
+        .route("/consultation", post(add_consultation))
+        .route("/doctor-profile/{doctor_id}", get(get_doctor_profile))
+        // do we really want this? or should we go with the email approach
+        // .route("/doctor-profile", post(set_doctor_profile))
+        .route("/purchase", get(get_purchases))
+        .route("/purchase", post(add_purchase))
+        .route("/user", get(get_user))
+        .route("/user-detail", get(get_user_detail))
+        .route("/user-detail", put(set_user_detail))
+        .route("/user-measurement", get(get_user_measurements))
+        .route("/user-measurement", post(add_user_measurement))
+        .route("/diagnosis/{consultation_id}", get(get_diagnoses))
+        .route("/symptom/{consultation_id}", get(get_symptoms))
+        .route("/prescription/{consultation_id}", get(get_prescriptions))
+        .route("/login", post(auth::email::login))
+        .route("/register", post(auth::email::register))
+        .route("/logout", post(auth::logout))
+        .route("/request-nonce", get(request_nonce))
+        .layer(cors)
+        .with_state(state)
+}
--- src/main.rs
+++ src/main.rs
@@ -21,71 +21,15 @@
 //    - pakai informasi KTP (NIK) dan Nomor Telp
 //    - bisa tambah informasi kesehatan lain (berat badan, tinggi, alergi, dll)
 
-mod auth;
-mod canonical_json;
-mod error;
-mod model;
-mod protocol;
-mod route;
-mod schema;
-
-use crate::route::{
-    allergy::{add_allergy, get_allergies, remove_allergy},
-    consultation::{add_consultation, get_consultations},
-    doctor_profile::{get_doctor_profile, set_doctor_profile},
-    purchase::{add_purchase, get_purchases},
-    request_nonce,
-    user::get_user,
-    user_detail::{get_user_detail, set_user_detail},
-    user_measurement::{add_user_measurement, get_user_measurements},
-};
-
-use axum::{
-    extract::FromRef,
-    routing::{delete, get, post, put},
-};
-use protocol::Nonce;
-use route::consultation::{get_diagnoses, get_prescriptions, get_symptoms};
-use std::time::Duration;
-use tracing::info;
-use uuid::Uuid;
-
-use axum::Router;
+use medigram_back::AppState;
 use moka::sync::Cache;
 use sqlx::Pool;
 use sqlx::postgres::Postgres;
-use tower_http::cors::{Any, CorsLayer};
+use std::time::Duration;
+use tracing::info;
 use tracing_subscriber::{
     EnvFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt,
 };
-
-// 7d
-const NONCE_TTL: Duration = Duration::from_secs(7 * 24 * 60 * 60);
-
-#[derive(Clone)]
-struct AppState {
-    nonce_cache: Cache<Nonce, ()>,
-    db_pool: Pool<Postgres>,
-    recognized_session_id: Cache<String, Uuid>,
-}
-
-impl FromRef<AppState> for Cache<String, Uuid> {
-    fn from_ref(input: &AppState) -> Self {
-        input.recognized_session_id.clone()
-    }
-}
-
-impl FromRef<AppState> for Pool<Postgres> {
-    fn from_ref(input: &AppState) -> Self {
-        input.db_pool.clone()
-    }
-}
-
-impl FromRef<AppState> for Cache<Nonce, ()> {
-    fn from_ref(input: &AppState) -> Self {
-        input.nonce_cache.clone()
-    }
-}
 
 #[tokio::main]
 async fn main() {
@@ -116,36 +60,7 @@ async fn main() {
             .build(),
     };
 
-    let cors = CorsLayer::new()
-        .allow_origin(Any)
-        .allow_methods(Any)
-        .allow_headers(Any);
-
-    let app = Router::new()
-        .route("/allergy", get(get_allergies))
-        .route("/allergy", post(add_allergy))
-        .route("/allergy", delete(remove_allergy))
-        .route("/consultation", get(get_consultations))
-        .route("/consultation", post(add_consultation))
-        .route("/doctor-profile/{doctor_id}", get(get_doctor_profile))
-        // do we really want this? or should we go with the email approach
-        // .route("/doctor-profile", post(set_doctor_profile))
-        .route("/purchase", get(get_purchases))
-        .route("/purchase", post(add_purchase))
-        .route("/user", get(get_user))
-        .route("/user-detail", get(get_user_detail))
-        .route("/user-detail", put(set_user_detail))
-        .route("/user-measurement", get(get_user_measurements))
-        .route("/user-measurement", post(add_user_measurement))
-        .route("/diagnosis/{consultation_id}", get(get_diagnoses))
-        .route("/symptom/{consultation_id}", get(get_symptoms))
-        .route("/prescription/{consultation_id}", get(get_prescriptions))
-        .route("/login", post(auth::email::login))
-        .route("/register", post(auth::email::register))
-        .route("/logout", post(auth::logout))
-        .route("/request-nonce", get(request_nonce))
-        .layer(cors)
-        .with_state(state);
+    let app = medigram_back::app(state);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
